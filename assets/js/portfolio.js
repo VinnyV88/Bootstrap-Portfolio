913dc519a81fdb54c23ebbd597e5309b109b38d8
Vincent Visconti <vinnyv88@gmail.com>
2018-02-08 09:25:15 -0500

    insert technologies to display for each project. enhance overlay.

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -7,7 +7,7 @@ var portfolio = [
         caption: "My Showspace",
         githubLink: "https://github.com/VinnyV88/virtual-designer#vivid",
         githubPrivate: true,
-        technology: ["JavaScript","HTML5 (Canvas)","Fabric.js (API to manage canvas objects/layers)","jQuery","AJAX","CSS/Bootstrap","FileSaver","Validator","Spectrum (color picker)","Moment.js", "Express.js","Node.js","Custom ORM","MySQL db", "Multer","Jimp","cookie-parser","body-parser","express-session"],
+        technology: ["HTML5 (Canvas)","Fabric.js (Canvas Management Library)","jQuery","AJAX","Node.js","Express.js","MySQL db"],
         synopsis: "A web app that allows users to design virtual rooms for multiple purposes. As an artist, to display your artwork in a virtual showroom. As a home-owner, to design a new interior project with artwork, furniture/decors, paint color. As a furniture company, to allow users to try your furniture in their virtual room prior to purchase. As a realtor, to virtual furnish and decorate images of vacant rooms."
     },
     roadTrip = {
@@ -16,6 +16,7 @@ var portfolio = [
         caption: "Ulitmate Road Trip Planner",
         githubLink: "https://github.com/VinnyV88/Road-Trip-Planner#the-ultimate-road-trip-planner",
         githubPrivate: false,
+        technology: ["jQuery","AJAX", "Google Maps API (Direction Services, Places, Autocomplete for Addresses)", "GeoNames API (City/Town Names)", "DarkSkies API (Weather Forecast)", "Bitly API (Directions URL Shortner)"],
         synopsis: "The Ultimate Road Trip Planner is an app where the user enters in a starting location and ending location to plan a road trip. The app makes suggestions for points of interest like Hotels/Motels and restaurants. It will also provide weather forecasts for key cities along the route to enhance the road trip plan. The app uses Geonames, Google maps, Dark Skies to plan the route and provide the key information."
     },
     starWars = {
@@ -24,6 +25,7 @@ var portfolio = [
         caption: "Star Wars RPG Game",
         githubLink: "https://github.com/VinnyV88/week-4-game#star-wars-rpg",
         githubPrivate: false,
+        technology: ["Game Flow Logic","jQuery","Adobe Shoebox (Extract sprites from sprite sheets)","Photoshop (modify/create individual sprites)","Audacity (Record and cut up sound effects)","sprite-animate.js (Character Animation)"],
         synopsis: "Multi-level RPG game where you can play as or fight against your favorite characters from the original Star Wars trilogy. Each level has their own set of characters, environments, battlefield action animations and sound effects. Defeat all opponents with each character to advance to the next level. Be sure to check out the Help section of the app for instructions on how to play and for tips on how to win."
     },
     hangman = {
@@ -32,6 +34,7 @@ var portfolio = [
         caption: "Hangman",
         githubLink: "https://github.com/VinnyV88/week-3-game#hangman-breaking-bad-edition",
         githubPrivate: false,
+        technology: ["Game Flow Logic","jQuery","Photoshop","Audacity (Record and cut up sound effects)"],
         synopsis: "A hangman game that randomly chooses a word or phrase related to AMC's TV series Breaking Bad. You play by guessing letters by pressing letter keys on the keyboard. Letter hits and misses are accompanied by CSS generated animations and sound effects from the show. A GIF and a sound byte related to the word/phrase are played when a word is solved. If you are unable to solve the word/phrase within the alloted guesses, the animated \"hangman\" (Heisenberg) gets hung out to dry."
     },
     mongoNews = {
@@ -40,6 +43,7 @@ var portfolio = [
         caption: "Mong-Oh-Cheerio News",
         githubLink: "https://github.com/VinnyV88/mongo-cheerio-news-scraper#mongo-cheerio-news-scraper",
         githubPrivate: false,
+        technology: ["Node.js","Express.js","express-handlebars", "mongo db", "mongoose","cheerio (web scraping)"],
         synopsis: "An app that lets users leave comments on the latest (web scraped) news using Mongoose, Cheerio, and Express"
     },
     sqlzdBurger = {
@@ -48,6 +52,7 @@ var portfolio = [
         caption: "King Burger Eating",
         githubLink: "https://github.com/VinnyV88/sequelizedBurger",
         githubPrivate: false,
+        technology: ["Node.js","Express.js","express-handlebars","MySQL db","sequelize" ],
         synopsis: "An app that demonstrates the use of a Sequelized MySQL database, using table associations and model validations along with Node, Express, and Handlebars"
     },
     friendFinder = {
@@ -56,6 +61,7 @@ var portfolio = [
         caption: "Friend Finder",
         githubLink: "https://github.com/VinnyV88/FriendFinder#friendfinder",
         githubPrivate: false,
+        technology: ["jQuery","AJAX","Node.js","Express.js","JSON", "Krita (graphics editor)", "validator.js"],
         synopsis: "A compatibility-based 'FriendFinder' application. This full-stack site takes in results of a user survey, then compares their answers with those from other users. The app will then display the name and picture of the user with the best overall match."
     },
     trainTracker = {
@@ -64,6 +70,7 @@ var portfolio = [
         caption: "Firebase Train Tracker",
         githubLink: "https://github.com/VinnyV88/fbTrainTracker#train-tracker",
         githubPrivate: false,
+        technology: ["Firebase","OAuth","jQuery","timepicker.js","moment.js"],
         synopsis: "The Train Tracker app allows visitors to view train schedule information of trains that were entered by users. Registered and login users have the ability to enter new train information, edit or delete existing trains. Next arriving train information is updated every minute, as indicated by the flip clock. Users have the option to register and login with either their Google or Github accounts."
     },
     trivia = {
@@ -72,6 +79,7 @@ var portfolio = [
         caption: "Trivia Game",
         githubLink: "https://github.com/VinnyV88/TriviaGame#trivia-game-wwewwf-edition",
         githubPrivate: false,
+        technology: ["jQuery","AJAX","GIPHY API","Photoshop","Audacity (Record and cut up sound effects)"],
         synopsis: "A trivia game that randomly chooses a question related to WWE/WWF \"Attitude\" era. The game will ask you 15 questions and display a summary at the end of how you did. You will have 20 seconds to answer each question. After a question is completed, a GIF retrieved via the GIPHY API will be displayed related to correct answer."
     }
 ];
@@ -81,16 +89,16 @@ var $row;
 for (var i = 0; i < portfolio.length; i++) {
 
     var $div = $('<div>').addClass("col-md-6 portfolio-flex-container").attr("data-emergence", "hidden");
-    var $ovlH2 = $('<h2>').text(portfolio[i].caption);
+    // var $ovlH2 = $('<h2>').text(portfolio[i].caption);
     // var $ovlP = $('<p>').text(portfolio[i].caption);
-    var $ul = $('<ul>');
+    var $ul = $('<ul>').attr("style", "text-align: left;");
     if (portfolio[i].technology) {
     portfolio[i].technology.forEach(function(tech) {
         var $li = $('<li>').text(tech);
         $ul.append($li);
     });
     }
-    var $overlay = $('<div>').addClass("portfolio-overlay").append($ovlH2).append($ul);
+    var $overlay = $('<div>').addClass("portfolio-overlay").append($ul);
     var $img = $('<img>').addClass('img-responsive link-ani flex-item-port-img').attr('src', portfolio[i].imgSrc).attr('alt', portfolio[i].caption);
     var $a = $('<a>').attr('href', portfolio[i].link).attr('target', '_blank');
     var $caption = $('<div>').addClass('flex-item-caption-hdr').text(portfolio[i].caption);
