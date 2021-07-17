492e5e11fcdc4788814aff2909a584901fac8c39
Vincent Laptop <vincent@vividwebsolutions.net>
2021-07-17 19:38:39 -0400

    update portfolio

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
-
+    var shipped = [
+        srspf = {
+            link: "https://sound-recording.org/",
+            imgSrc: "assets/images/SRSPF770x514.png",
+            caption: "Sound Recording Special Payments Fund",
+            githubLink: "https://sound-recording.org/",
+            githubPrivate: true,
+            synopsis: "Designed, Developed and Launched a new website application for the Sound Recording Special Payments Fund. The website features public pages that communicate essential information about the Fund. The site also features a private member section that allows the Fund's participants to register for access to maintain their personal information, view documents related to their payments received from the Fund (e.g., statements and tax documents) and also submit electronic forms. The Fund has a private administration section where they can administer their website users, and a Forms Processing system which allows their employees to review and process electronic forms submitted by their users. Participant information is automatically synchronized between the website database and the Fund's backend database, which relieves the Fund of manual intervention for data updates."
+        },
+    ];    
 var portfolio = [
     myshowspace = {
         link: "https://www.myshowspace.com/",
@@ -10,15 +19,15 @@ var portfolio = [
         technology: ["HTML5 (Canvas)","Fabric.js (Canvas Management Library)","jQuery","AJAX","JSON","Node.js","Express.js","MySQL db"],
         synopsis: "A web app that allows users to design virtual rooms for multiple purposes. As an artist, to display your artwork in a virtual showroom. As a home-owner, to design a new interior project with artwork, furniture/decors, paint color. As a furniture company, to allow users to try your furniture in their virtual room prior to purchase. As a realtor, to virtual furnish and decorate images of vacant rooms."
     },
-    roadTrip = {
-        link: "road_trip_planner/index.html",
-        imgSrc: "assets/images/urtp385x257.png",
-        caption: "Ultimate Road Trip Planner",
-        githubLink: "https://github.com/VinnyV88/Road-Trip-Planner#the-ultimate-road-trip-planner",
-        githubPrivate: false,
-        technology: ["jQuery","AJAX", "Google Maps API (Direction Services, Places, Autocomplete for Addresses)", "GeoNames API (City/Town Names)", "DarkSkies API (Weather Forecast)", "Bitly API (Directions URL Shortner)"],
-        synopsis: "The Ultimate Road Trip Planner is an app where the user enters in a starting location and ending location to plan a road trip. The app makes suggestions for points of interest like Hotels/Motels and restaurants. It will also provide weather forecasts for key cities along the route to enhance the road trip plan. The app uses Geonames, Google maps, Dark Skies to plan the route and provide the key information."
-    },
+    // roadTrip = {
+    //     link: "road_trip_planner/index.html",
+    //     imgSrc: "assets/images/urtp385x257.png",
+    //     caption: "Ultimate Road Trip Planner",
+    //     githubLink: "https://github.com/VinnyV88/Road-Trip-Planner#the-ultimate-road-trip-planner",
+    //     githubPrivate: false,
+    //     technology: ["jQuery","AJAX", "Google Maps API (Direction Services, Places, Autocomplete for Addresses)", "GeoNames API (City/Town Names)", "DarkSkies API (Weather Forecast)", "Bitly API (Directions URL Shortner)"],
+    //     synopsis: "The Ultimate Road Trip Planner is an app where the user enters in a starting location and ending location to plan a road trip. The app makes suggestions for points of interest like Hotels/Motels and restaurants. It will also provide weather forecasts for key cities along the route to enhance the road trip plan. The app uses Geonames, Google maps, Dark Skies to plan the route and provide the key information."
+    // },
     starWars = {
         link: "star_wars/index.html",
         imgSrc: "assets/images/Star_Wars385x257.png",
@@ -37,33 +46,33 @@ var portfolio = [
         technology: ["Game Flow Logic","jQuery","Photoshop","Audacity (Record and cut up sound effects)"],
         synopsis: "A hangman game that randomly chooses a word or phrase related to AMC's TV series Breaking Bad. You play by guessing letters by pressing letter keys on the keyboard. Letter hits and misses are accompanied by CSS generated animations and sound effects from the show. A GIF and a sound byte related to the word/phrase are played when a word is solved. If you are unable to solve the word/phrase within the alloted guesses, the animated \"hangman\" (Heisenberg) gets hung out to dry."
     },
-    mongoNews = {
-        link: "https://mong-oh-cheerio.herokuapp.com/",
-        imgSrc: "assets/images/mongo-news385x257.png",
-        caption: "Mong-Oh-Cheerio News",
-        githubLink: "https://github.com/VinnyV88/mongo-cheerio-news-scraper#mongo-cheerio-news-scraper",
-        githubPrivate: false,
-        technology: ["Node.js","Express.js","express-handlebars", "mongo db", "mongoose","cheerio (web scraping)"],
-        synopsis: "An app that lets users leave comments on the latest (web scraped) news using Mongoose, Cheerio, and Express"
-    },
-    sqlzdBurger = {
-        link: "https://sqlzd-burger-app.herokuapp.com/burgers",
-        imgSrc: "assets/images/sqlzd-burger-eating385x257.png",
-        caption: "King Burger Eating",
-        githubLink: "https://github.com/VinnyV88/sequelizedBurger",
-        githubPrivate: false,
-        technology: ["Node.js","Express.js","express-handlebars","MySQL db","sequelize" ],
-        synopsis: "An app that demonstrates the use of a Sequelized MySQL database, using table associations and model validations along with Node, Express, and Handlebars"
-    },
-    friendFinder = {
-        link: "https://vivi-friend-finder.herokuapp.com/",
-        imgSrc: "assets/images/FriendFinder385x257.png",
-        caption: "Friend Finder",
-        githubLink: "https://github.com/VinnyV88/FriendFinder#friendfinder",
-        githubPrivate: false,
-        technology: ["jQuery","AJAX","Node.js","Express.js","JSON", "Krita (graphics editor)", "validator.js"],
-        synopsis: "A compatibility-based 'FriendFinder' application. This full-stack site takes in results of a user survey, then compares their answers with those from other users. The app will then display the name and picture of the user with the best overall match."
-    },
+    // mongoNews = {
+    //     link: "https://mong-oh-cheerio.herokuapp.com/",
+    //     imgSrc: "assets/images/mongo-news385x257.png",
+    //     caption: "Mong-Oh-Cheerio News",
+    //     githubLink: "https://github.com/VinnyV88/mongo-cheerio-news-scraper#mongo-cheerio-news-scraper",
+    //     githubPrivate: false,
+    //     technology: ["Node.js","Express.js","express-handlebars", "mongo db", "mongoose","cheerio (web scraping)"],
+    //     synopsis: "An app that lets users leave comments on the latest (web scraped) news using Mongoose, Cheerio, and Express"
+    // },
+    // sqlzdBurger = {
+    //     link: "https://sqlzd-burger-app.herokuapp.com/burgers",
+    //     imgSrc: "assets/images/sqlzd-burger-eating385x257.png",
+    //     caption: "King Burger Eating",
+    //     githubLink: "https://github.com/VinnyV88/sequelizedBurger",
+    //     githubPrivate: false,
+    //     technology: ["Node.js","Express.js","express-handlebars","MySQL db","sequelize" ],
+    //     synopsis: "An app that demonstrates the use of a Sequelized MySQL database, using table associations and model validations along with Node, Express, and Handlebars"
+    // },
+    // friendFinder = {
+    //     link: "https://vivi-friend-finder.herokuapp.com/",
+    //     imgSrc: "assets/images/FriendFinder385x257.png",
+    //     caption: "Friend Finder",
+    //     githubLink: "https://github.com/VinnyV88/FriendFinder#friendfinder",
+    //     githubPrivate: false,
+    //     technology: ["jQuery","AJAX","Node.js","Express.js","JSON", "Krita (graphics editor)", "validator.js"],
+    //     synopsis: "A compatibility-based 'FriendFinder' application. This full-stack site takes in results of a user survey, then compares their answers with those from other users. The app will then display the name and picture of the user with the best overall match."
+    // },
     trainTracker = {
         link: "https://traintracker-d4450.firebaseapp.com/",
         imgSrc: "assets/images/trainTracker385x257.png",
@@ -84,13 +93,46 @@ var portfolio = [
     }
 ];
 
-var $row;
+for (var i = 0; i < shipped.length; i++) {
+
+    var $shippedLi = $('<li>');
+    var $div = $('<div>').addClass("shipped-flex-container").attr("data-emergence", "hidden");
+    var $ul = $('<ul>').attr("style", "text-align: left;");
+    if (shipped[i].technology) {
+    shipped[i].technology.forEach(function(tech) {
+        var $li = $('<li>').text(tech);
+        $ul.append($li);
+    });
+    }
+    var $overlay = $('<div>').addClass("shipped-overlay").append($ul);
+    var $img = $('<img>').addClass('img-responsive link-ani flex-item-shipped-img').attr('src', shipped[i].imgSrc).attr('alt', shipped[i].caption);
+    var $a = $('<a>').attr('href', shipped[i].link).attr('target', '_blank');
+    var $demoLink = $('<a>').addClass('demo-link').attr('href', shipped[i].link).attr('target', '_blank').text(shipped[i].caption);
+    var $caption = $('<div>').addClass('flex-item-caption-hdr').html($demoLink);
+    var $captionBody = $('<div>').addClass('flex-item-caption-body-shipped').text(shipped[i].synopsis);
+    var $captionLink = $('<div>').addClass('flex-item-caption-button');
+    if (shipped[i].githubPrivate) var $aButton = $('<a>').attr('href', shipped[i].githubLink).attr('target', '_blank').addClass('caption-button').text("View the Website");
+    else var $aButton = $('<a>').attr('href', shipped[i].githubLink).attr('target', '_blank').addClass('caption-button').text("Read More on GitHub");
+    
+    $captionLink.append($aButton);
+
+    $a.html($img);
+    $div.append($a);
+    $div.append($overlay);    
+
+    $div.append($caption);
+    $div.append($captionBody);
+    $div.append($captionLink);
+
+    $shippedLi.append($div);
+    $('.shipped-well').append($shippedLi);
+};
 
 for (var i = 0; i < portfolio.length; i++) {
 
-    var $div = $('<div>').addClass("col-md-4 portfolio-flex-container").attr("data-emergence", "hidden");
-    // var $ovlH2 = $('<h2>').text(portfolio[i].caption);
-    // var $ovlP = $('<p>').text(portfolio[i].caption);
+    var $portLi = $('<li>');
+
+    var $div = $('<div>').addClass("portfolio-flex-container").attr("data-emergence", "hidden");
     var $ul = $('<ul>').attr("style", "text-align: left;");
     if (portfolio[i].technology) {
     portfolio[i].technology.forEach(function(tech) {
@@ -118,17 +160,10 @@ for (var i = 0; i < portfolio.length; i++) {
     $div.append($captionBody);
     $div.append($captionLink);
 
-    if ((i % 3) === 0) {
-        $row = $('<div>').addClass('row');
-        $row.append($div);
+    $portLi.append($div);
 
-        //if we have an odd number of portfolio elements and we are 
-        //processing the last one, write the div
-        if ((portfolio.length - i) === 1) $('.port-well').append($row);
-    } else {
-        $row.append($div);
-        $('.port-well').append($row);
-    }
+    $('.port-well').append($portLi);
 };
+
 
 });
